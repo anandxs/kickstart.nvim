23d7f480cd0b9c41123923b4eb59a8ace32eb6cb
Anand S <anandsatheeshk@gmail.com>
2024-04-30 22:17:52 +0530

    disable word wrap | add dockerls

--- init.lua
+++ init.lua
@@ -21,6 +21,7 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
+vim.opt.wrap = false
 
 -- [[ Basic Keymaps ]]
 vim.keymap.set('i', 'jk', '<Esc>')
@@ -399,6 +400,7 @@ require('lazy').setup({
         tsserver = {},
         cssls = {},
         emmet_ls = {},
+        dockerls = {},
         --
 
         lua_ls = {
